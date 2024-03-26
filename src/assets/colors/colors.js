fad28da27f49f15faf3bd3f3114c67a1855fd5d4
timdeckel <timdeckel@gmail.com>
2024-03-26 22:41:42 +0100

    added colors.js and added a color scheme for bot chaos and imperium

---
+++ src/assets/colors/colors.js
@@ -0,0 +1,36 @@
+export const COLORS = {
+  imperialPrimary: "#9AC0C6",
+  imperialSecondary: "#DFEDEE",
+  imperialTertiery: "#436C77",
+  chaosPrimary: "#C72B07",
+  chaosSecondary: "#450C05",
+  chaosTertiery: "#FF7C36",
+}
+
+const IMP__COLORS = {
+  lightFrost: "#F3F8F8",
+  paleSilver: "#DFEDEE",
+  softBlueGray: "#C3DADE",
+  mistyTeal: "#9AC0C6",
+  dustyCerulean: "#699DA7",
+  slateAzure: "#4D818D",
+  shadowedSteel: "#436C77",
+  deepGraphite: "#3B5963",
+  nightfallCharcoal: "#364C54",
+  midnightAsh: "#29373D",
+  inkyObsidian: "#1D2A2F"
+}
+
+const CHAOS__COLORS = {
+  peachPuff: "#FFF5EC",
+  apricot: "#FFE9D4",
+  sandyBrown: "#FFCEA8",
+  mustard: "#FFAB70",
+  carrotOrange: "#FF7C36",
+  tangerine: "#FF590F",
+  scarlet: "#F03E06",
+  burntSienna: "#C72B07",
+  rustyRed: "#9E230E",
+  mahogany: "#771E0E",
+  maroon: "#450C05"
+}
